d60777ebeb51c122258c9edd49cbf5c8f377592b
yuul2 <77230028+yuul2@users.noreply.github.com>
2021-11-24 22:29:25 +0900

    recently

--- js/yuul.js
+++ js/yuul.js
@@ -1,7 +1,4 @@
 $(function(){
-    $('.send_btn').click(function(){
-        $('.thankyou_message').css('display','block');
-    });
     $('.close').click(function(){
         $('.thankyou_message').css('display','none');
     });
