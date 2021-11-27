06712f218f9f2f87139ede66e07cb441ace90f27
yuul2 <77230028+yuul2@users.noreply.github.com>
2021-11-27 20:00:14 +0900

    newstyle

--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 // 이미지 지도에서 마커가 표시될 위치입니다 
-var markerPosition  = new kakao.maps.LatLng(37.488360555432514, 127.00854038465968); 
+var markerPosition  = new kakao.maps.LatLng(501902.5, 1107913.75); 
 
 // 이미지 지도에 표시할 마커입니다
 // 이미지 지도에 표시할 마커는 Object 형태입니다
@@ -9,7 +9,7 @@ var marker = {
 
 var staticMapContainer  = document.getElementById('staticMap'), // 이미지 지도를 표시할 div  
     staticMapOption = { 
-        center: new kakao.maps.LatLng(37.488360555432514, 127.00854038465968), // 이미지 지도의 중심좌표
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 이미지 지도의 중심좌표
         level: 3, // 이미지 지도의 확대 레벨
         marker: marker // 이미지 지도에 표시할 마커 
     };    
